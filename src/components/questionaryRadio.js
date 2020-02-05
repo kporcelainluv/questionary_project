0757b26432e6b9d10015e2a461b4c1ab5937016f
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-02-05 17:09:16 +0300

    add onClick handler

--- src/components/questionaryRadio.js
+++ src/components/questionaryRadio.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-export const QuestionaryRadio = ({ id, name, value, text }) => {
+export const QuestionaryRadio = ({ id, name, value, text, handleOnClick }) => {
   return (
     <label htmlFor={id}>
       {text}
-      <input type="radio" id={id} name={name} value={value} />
+      <input
+        type="radio"
+        id={id}
+        name={name}
+        value={value}
+        onChange={() => {
+          return handleOnClick(name, value);
+        }}
+      />
     </label>
   );
 };
