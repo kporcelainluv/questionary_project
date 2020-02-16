52187e7e1cad8cae320cab0ff4f086b1fa4f6975
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-02-16 19:55:14 +0300

    Remove dead import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Form } from "./components/Form";
 import { List } from "./components/List";
 import { User } from "./components/User";
 import { AuthProvider } from "./Auth";
-import { firebaseApp } from "./base";
 import "firebase/auth";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
