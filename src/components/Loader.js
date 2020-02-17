4dd0ccfc6905bd2f1ae65277a10b7e0c4fcbbf36
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-02-17 14:40:54 +0300

    add new loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,12 +3,11 @@ import "../css/Loader.css";
 
 export const Loader = () => {
   return (
-    <div className="spin-container">
-      <div className="spin" id="loader"></div>
-      <div className="spin" id="loader2"></div>
-      <div className="spin" id="loader3"></div>
-      <div className="spin" id="loader4"></div>
-      <span id="text">LOADING...</span>
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
