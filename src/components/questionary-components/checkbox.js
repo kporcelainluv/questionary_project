f83fb92ab14e290ceb34ec8c3f5ac14b77ea2b11
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-02-22 15:50:01 +0300

    remove questionary components

--- src/components/questionary-components/checkbox.js
+++
@@ -1,3 +0,0 @@
-import React, { Fragment } from "react";
-import styled from "styled-components";
-import { MediaWidth } from "../../consts";
--- src/components/questionary-components/questionaryRadio.js
+++
@@ -1,4 +0,0 @@
-import React, { Fragment } from "react";
-import styled from "styled-components";
-
-import { MediaWidth } from "../../consts";
