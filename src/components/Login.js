dc58e5db9ed193eccc5a40eb6f993e42a7c51b07
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-02-25 15:00:05 +0300

    add error handling, refactor component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,56 +1,30 @@
-import React, { useCallback, useContext } from "react";
+import React, { useContext, useState } from "react";
 import { withRouter, Redirect } from "react-router";
 import * as firebase from "firebase/app";
 
-import { firebaseApp } from "../base";
 import { AuthContext } from "./Auth.js";
+import { Error } from "./Error";
 
-const Login = ({ history }) => {
-  useCallback(
-    async event => {
-      event.preventDefault();
-      const { email, password } = event.target.elements;
-      try {
-        await firebaseApp
-          .auth()
-          .signInWithEmailAndPassword(email.value, password.value);
-        history.push("/");
-      } catch (error) {
-        alert(error);
-      }
-    },
-    [history]
-  );
+const allowedUsers = ["zhukovaksusha@gmail.com", "muazhukova@gmail.com"];
 
+export const Login = withRouter(() => {
+  const [error, setError] = useState(false);
   const handleGoogleAuth = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
 
     firebase
       .auth()
       .signInWithPopup(provider)
-      .then(function(result) {
+      .then(result => {
         const user = result.user;
-        console.log(user.email);
-        if (
-          user.email !== "zhukovaksusha@gmail.com" &&
-          user.email !== "muazhukova@gmail.com"
-        ) {
+        if (!allowedUsers.includes(user.email)) {
           firebase
             .auth()
             .signOut()
-            .then(
-              function() {
-                console.log("Signed Out");
-              },
-              function(error) {
-                console.error("Sign Out Error", error);
-              }
-            );
+            .catch(console.error);
         }
       })
-      .catch(function(error) {
-        console.log(error);
-      });
+      .catch(() => setError(true));
   };
 
   const { currentUser } = useContext(AuthContext);
@@ -59,16 +33,20 @@ const Login = ({ history }) => {
     return <Redirect from="*" to="/list/" />;
   }
 
+  if (error) {
+    return (
+      <section>
+        <Error />
+      </section>
+    );
+  }
+
   return (
     <section>
-      <h2 className="login_heading">
-        Авторизуйтесь в Google для получения доступа
-      </h2>
-      <button className="button-long " onClick={handleGoogleAuth}>
+      <h2 className="login_heading">Авторизуйтесь для получения доступа</h2>
+      <button className="button-long" onClick={handleGoogleAuth}>
         Авторизоваться в Google
       </button>
     </section>
   );
-};
-
-export default withRouter(Login);
+});
