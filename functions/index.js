0a09330218675826a0e0da2e30945d2736fabad9
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-02-25 15:28:27 +0300

    refactor website link for telegram bot

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,13 @@
 const functions = require("firebase-functions");
 
 const createLink = ({ id, name }) => {
-  const text = `${name} заполнила форму, посмотреть можно по ссылке https://questionaryproject.now.sh/user/${id}`;
-
+  const text = `${name} заполнила форму, посмотреть можно по ссылке https://zhukovairina.now.sh/user/${id}`;
+  const reminderText = `https://zhukovairina.now.sh/ - ссылка на форму; https://zhukovairina.now.sh/list - список пользователей, заполнивших форму`;
   return encodeURI(
     `https://api.telegram.org/bot${
       functions.config().token.id
-    }/sendMessage?chat_id=${functions.config().chatid.id}&text=${text}`
+    }/sendMessage?chat_id=${functions.config().chatid.id}&text=${text +
+      reminderText}`
   );
 };
 
