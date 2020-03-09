ad183a3a03a7df9436ef1e3ddf317c8147e35ff6
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-03-09 11:36:46 +0300

    add fetch package, add promise to send messages to several chat ids

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,12 @@
 const functions = require("firebase-functions");
+const fetch = require("node-fetch");
 
-const createLink = ({ id, name }) => {
-  const text = `${name} заполнила форму, посмотреть можно по ссылке https://zhukovairina.now.sh/user/${id}`;
-  const reminderText = `https://zhukovairina.now.sh/ - ссылка на форму; https://zhukovairina.now.sh/list - список пользователей, заполнивших форму`;
+const createLink = ({ id, name, chatId }) => {
+  const text = `${name} заполнила форму, посмотреть можно по ссылке https://zhukovairina.now.sh/user/${id} `;
   return encodeURI(
     `https://api.telegram.org/bot${
       functions.config().token.id
-    }/sendMessage?chat_id=${functions.config().chatid.id}&text=${text +
-      reminderText}`
+    }/sendMessage?chat_id=${chatId}&text=${text }`
   );
 };
 
@@ -17,8 +16,12 @@ exports.onAddingNewUser = functions
   .onCreate((snap, context) => {
     const name = snap.data()["name"];
     const id = snap.data()["id"];
+    const sendMessageToMe = fetch(createLink({ id, name, chatId: "98266189" }));
+    const sendMessageToMom = fetch(
+      createLink({ id, name, chatId: "132097728" })
+    );
 
-    fetch(createLink({ id, name }))
+    Promise.all([sendMessageToMe, sendMessageToMom])
       .then(res => {
         return res;
       })
